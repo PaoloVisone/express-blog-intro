152714ce914bd896db26070624599ce3c79d00b1
PaoloVisone <polvis.pv.22@gmail.com>
2025-02-04 17:13:56 +0100

    [feat] add static asset

--- server.js
+++ server.js
@@ -3,12 +3,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// Rotta 
+// Asset statici
+app.use(express.static('public'));
+
+// Rotta '/'
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Server del mio blog')
+})
+
+// Rotta '/bacheca'
+app.get('/bacheca', (req, res) => {
+    const menu = [
+
+    ]
 })
 
 app.listen(port, () => {
-    console.log(`Server del mio blog`)
+    console.log(`Example app listening on port ${port}`)
 })
 1
